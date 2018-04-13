e3c56c7d5d0d2dfcf0dccf42f5ce21445100a339
JS-Quinn <josephshannonquinn@Gmail.com>
2018-04-13 08:23:34 -0400

    edited mongo connection to mongoose

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ var Article = require("./models/Article");
 
 // Connect mongoose to MongoDB
 
-mongoose.connect("mongodb://localhost/scraper");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 // Initialize Express
 
