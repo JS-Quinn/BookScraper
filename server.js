a9e5ba36ba3e1385cac22a46b93dde884249c1de
JS-Quinn <josephshannonquinn@Gmail.com>
2018-04-14 12:21:58 -0400

    connection edit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
+  //useMongoClient: true
 });
 
 // Initialize Express
